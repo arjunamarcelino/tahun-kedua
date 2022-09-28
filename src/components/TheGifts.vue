f13911eae71e080bc6036fae071ca5efe49850f4
arjunamarcelino <arjuna.marcelino@gdn-commerce.com>
2022-09-28 18:15:14 +0700

    add vidio

--- src/components/TheGifts.vue
+++ src/components/TheGifts.vue
@@ -1,4 +1,18 @@
-<script setup></script>
 <template>
-  <h3>Gidts</h3>
+  <h3 style="margin-bottom: 30px">
+    bukan hadiah sih, tapi ada sedikit video tentang kita satu tahun kebelakang
+    hadiahnya nyusul nanti bulan Desember, hehehehe
+  </h3>
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/x2k21sYDkYA"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+  <h3 style="margin-top: 30px">SEMOGA SUKAA!!!</h3>
 </template>
+
+<script></script>
